8808b6726782f8ddf31682b11b8fe8d50ae145ab
Dima Kostenich <dima.kostenich@gmail.com>
2018-05-27 01:33:04 +0200

    Remove ALPHA

---
+++ journalugo/Widgets/BadgeWidget.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeWidget.swift
+//  journalugo
+//
+//  Created by Dima Kostenich on 5/26/18.
+//  Copyright © 2018 Hyped Inc. All rights reserved.
+//
+
+import UIKit
+
+class BadgeWidget: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
